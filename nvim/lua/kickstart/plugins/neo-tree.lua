bc52e4a81cf641a5d1bf4ed9c860fb27a7960066
patil-suraj <surajp815@gmail.com>
2025-02-07 18:01:19 +0100

    make it fast

--- nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -5,46 +5,48 @@ return {
   { -- File explorer
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
+
+    lazy = true,
+    cmd = "Neotree",
+    keys = {
+      { "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Toggle Explorer" },
+      { "<leader>bf", "<cmd>Neotree buffers<CR>", desc = "Toggle Buffers" },
+      { "<leader>gs", "<cmd>Neotree git_status<CR>", desc = "Git Status" },
+    },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
     },
-    config = function()
-      require("neo-tree").setup({
-        close_if_last_window = true,
-        enable_git_status = true,
-        enable_diagnostics = true,
-        filesystem = {
-          follow_current_file = {
-            enabled = true,
-          },
-          filtered_items = {
-            visible = false,
-            hide_dotfiles = false,
-            hide_gitignored = false,
-          },
+    opts = {
+      close_if_last_window = true,
+      enable_git_status = true,
+      enable_diagnostics = true,
+      filesystem = {
+        follow_current_file = {
+          enabled = true,
         },
-        window = {
-          width = 30,
-          mappings = {
-            ["<space>"] = "none",
-          },
+        filtered_items = {
+          visible = false,
+          hide_dotfiles = false,
+          hide_gitignored = false,
         },
-        default_component_configs = {
-          indent = {
-            with_expanders = true,
-            expander_collapsed = "",
-            expander_expanded = "",
-            expander_highlight = "NeoTreeExpander",
-          },
+        use_libuv_file_watcher = false,
+      },
+      window = {
+        width = 30,
+        mappings = {
+          ["<space>"] = "none",
         },
-      })
-      
-      -- Keymaps
-      vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', {})
-      vim.keymap.set('n', '<leader>bf', ':Neotree buffers<CR>', {})
-      vim.keymap.set('n', '<leader>gs', ':Neotree git_status<CR>', {})
-    end,
+      },
+      default_component_configs = {
+        indent = {
+          with_expanders = true,
+          expander_collapsed = "",
+          expander_expanded = "",
+          expander_highlight = "NeoTreeExpander",
+        },
+      },
+    },
   }
 }
