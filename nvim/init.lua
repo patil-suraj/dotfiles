1b371bdb9ff33004a6e524f4c898c6b28185d88f
patil-suraj <surajp815@gmail.com>
2025-02-07 17:30:43 +0100

    up

--- nvim/init.lua
+++ nvim/init.lua
@@ -252,7 +252,7 @@ require('lazy').setup({
 
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
-    event = 'VimEnter',
+    -- event = 'VimEnter',
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
